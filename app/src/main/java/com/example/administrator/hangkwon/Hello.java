7495328d1871e6538614ec7ddff1854ff74bb38a
JackChao <875636632@qq.com>
2017-03-30 00:08:05 +0800

    修改了Hellol类

--- app/src/main/java/com/example/administrator/hangkwon/Hello.java
+++ app/src/main/java/com/example/administrator/hangkwon/Hello.java
@@ -2,14 +2,17 @@ package com.example.administrator.hangkwon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 /**
  * Createdby Administrator on 2017/3/30.
  */
 
-public class Hello extends Activity{
+public class Hello extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Toast.makeText(Hello.this, "fafa", Toast.LENGTH_SHORT).show();
     }
 }
