3571862980be7217eae6166d44c0c5c5b56a483b
JackChao <875636632@qq.com>
2017-03-30 00:13:07 +0800

    修改了Hellol类5

--- app/src/main/java/com/example/administrator/hangkwon/Hello.java
+++ app/src/main/java/com/example/administrator/hangkwon/Hello.java
@@ -17,5 +17,6 @@ public class Hello extends Activity {
         Toast.makeText(Hello.this, "fafa", Toast.LENGTH_SHORT).show();
         Toast.makeText(Hello.this, "fafa", Toast.LENGTH_SHORT).show();
         Toast.makeText(Hello.this, "fafa", Toast.LENGTH_SHORT).show();
+        Toast.makeText(Hello.this, "fafa", Toast.LENGTH_SHORT).show();
     }
 }
